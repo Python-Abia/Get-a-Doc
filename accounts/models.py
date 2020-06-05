f17a4d86d6698cae80bcb6069dd0d12e302231b4
= <=>
2020-06-05 14:07:28 +0100

    Finished building the UserProfile Model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile:
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='media/')
+    address = models.TextField()
+    phone = models.CharField(max_length=30)
+    is_doctor = models.BooleanField()
+
+    def __str__(self):
+        return user.first_name + " " + user.last_name
+
+    class Meta:
+        verbose_name_plural = 'User Profiles'
+        db_table = 'user_profiles'
+
+
+
