dd20971bc52f756af96334a6bb37ec78daf16a36
= <=>
2020-06-05 13:43:12 +0100

    restructured project

--- Gad-venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\ucktech\desktop\contribute\get_a_doc\gad-venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
