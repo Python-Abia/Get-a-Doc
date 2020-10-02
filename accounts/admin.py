f590d6a9065d41bf5af681e1597bf7ac305a6e00
PhysicistSouravDas <souravdas.sd20@gmail.com>
2020-10-02 23:05:41 +0530

    Registered UserProfile Model with features in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
+from .models import UserProfile
 
 
 # Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone', 'is_doctor')    # The value of 'list_display' must not be a ManyToManyField.
+    list_filter = ('is_doctor',)    # The value of 'list_filter' must be a list or tuple
+    search_fields = ('user',)
+    ordering = ('user',)
