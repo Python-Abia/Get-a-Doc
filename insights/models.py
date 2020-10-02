9685caf2b3a34308bc58ce04c2baa3a1b023f3c3
kalistos1 <ucktech1@gmail.com>
2020-10-02 16:54:48 +0100

    added conversation models and wrote the test"

--- insights/models.py
+++ insights/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    first_name = models.CharField(blank = False, max_length = 50)
+    last_name  = models.CharField(blank = False, max_length = 50)
+    subject    = models.CharField (blank = False, max_length = 20)
+    email      = models.EmailField(blank = False, max_length = 50)
+    text       = models.TextField(blank = False, max_length = 500)
+    
+    class Meta:
+        verbose_name_plural = "messages"
+        db_table = "messages"
+    
+    def __str__(self):
+        return self.email + " " + self.subject
+    
