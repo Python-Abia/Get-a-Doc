a06d611e7336cc08e6a36ced655202fa67147dc4
kalistos1 <ucktech1@gmail.com>
2020-12-01 17:01:23 +0100

    resolve issues #17 insight models @kscottz

--- get_a_doc/settings.py
+++ get_a_doc/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'comments',
     'inquiries',
     'conversations',
+    'insights',
 ]
 
 MIDDLEWARE = [
--- inquiries/urls.py
+++ inquiries/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .import views
+from . import views
 # urls patterns
 
 app_name = "inquiries"
